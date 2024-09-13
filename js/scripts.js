d4e21021fa2a21a6b96a74504f9ee6fc6c8ca4d6
AliBasit1030 <1ranaaliabdulbasit@gmail.com>
2024-09-13 07:02:46 +0500

    order page
    order page

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,10 @@ loadComponent('product-header', 'assets/components/ProductHeader.html');
 loadComponent('Product-features', 'assets/components/Products-features.html');
 loadComponent('faq', 'assets/components/faq-header.html');
 loadComponent('faqs', 'assets/components/faqs.html');
+loadComponent('order', 'assets/components/OrderHeader.html');
+loadComponent('order-milk', 'assets/components/OrderMilk.html');
+loadComponent('contact-header', 'assets/components/ContactHeader.html');
+loadComponent('get-in-touch', 'assets/components/get-in-touch.html');
+loadComponent('checkout-header', 'assets/components/checkout-header.html');
+loadComponent('checkout-form', 'assets/components/checkout-form.html');
+
